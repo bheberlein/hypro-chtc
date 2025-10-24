241f7477daa8cb74ce225c8feed009f4d514887e
Brendan Heberlein <bheberlein@wisc.edu>
2025-10-24 09:03:44 -0500

    [R/F] also remove empty err files

--- source/libradtran/cleanup.sh
+++ source/libradtran/cleanup.sh
@@ -4,5 +4,15 @@ STAGING=/staging/groups/townsend_airborne
 PREFIX=$STAGING/data/lut/raw
 
 if [[ $1 ]]; then
-  rm $PREFIX/${1}_*.out
+  (
+    shopt -s nullglob
+    for out in $PREFIX/${1}_*.out; do
+      # Remove libRadtran output
+      rm $out
+    done
+    for err in $PREFIX/${1}_*.err; do
+      # Only remove error file if it is empty
+      [[ ! -s "$FILE" ]] && rm $err
+    done
+  )
 fi
