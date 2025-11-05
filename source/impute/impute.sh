87d348585ef9f9c161d8ebdc5afca9b3be79b882
Brendan Heberlein <bheberlein@wisc.edu>
2025-11-05 11:17:40 -0600

    Add imputation job files
    The imputation workflow aims to detect & remove gross sensor artifacts.

---
+++ source/impute/impute.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+STAGING=/staging/groups/townsend_airborne
+
+ENVNAME=impute
+. utils/conda.sh
+conda_setup
+
+PACKAGE_DIR=$STAGING/source/packages
+HYPROTAR=hypro_1.0.1dev5.tar.gz
+
+cp $PACKAGE_DIR/$HYPROTAR ./
+source utils/archive.sh
+unpack $HYPROTAR
+
+# TODO: `numpy.tostring()` in `hypro.Radiometry`
+sed -i 's/.tostring()/.tobytes()/g' src/hypro/Radiometry.py
+
+make_importable $(pwd)/src/
+
+mkdir -p data/raw output/rdn
+
+# Resolve command-line arguments
+BASENAME=$1
+THRESHOLD=${2:-"0.10"}
+SENSOR=VNIR_1800_SN00840_FOVx2
+
+cp $STAGING/data/imputed/input/${BASENAME}_${SENSOR}_raw.* data/raw/
+
+python -m enspec.processing.workflows.impute --name $BASENAME --threshold $THRESHOLD
+
+OUTPUT_DIRECTORY=$STAGING/data/imputed/output
+mkdir -p $OUTPUT_DIRECTORY/$BASENAME
+cp -r output/* $OUTPUT_DIRECTORY/$BASENAME/
