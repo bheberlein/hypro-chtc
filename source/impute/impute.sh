eb377960b30aea627efabe85758f51b6db8187f0
Brendan Heberlein <bheberlein@wisc.edu>
2026-03-31 15:59:21 -0500

    [F] misc edits to `impute.sh` job executable

--- source/impute/impute.sh
+++ source/impute/impute.sh
@@ -24,15 +24,22 @@ mkdir -p data/raw output/rdn
 BASENAME=$1
 SENSOR=VNIR_1800_SN00840_FOVx2
 
+# Transfer input data
 cp $STAGING/data/imputed/input/${BASENAME}_${SENSOR}_raw.* data/raw/
 
+# Run gross linear sensor artifact correction workflow
 python -m enspec.processing.workflows.impute --name $BASENAME ${@:2}
 
+# Resolve output directory
 if [[ ! -v OUTPUT_DIRECTORY ]]; then
   OUTPUT_DIRECTORY=$STAGING/data/imputed/output
 fi
 
+echo "Copy to: $OUTPUT_DIRECTORY"
+
+# Transfer outputs
 mkdir -p $OUTPUT_DIRECTORY/$BASENAME
 cp -r output/* $OUTPUT_DIRECTORY/$BASENAME/
 
-rm -r $ENVDIR/ src/ data/ output/
+# Clean up
+rm -r $ENVDIR/ utils/ src/ data/ output/
